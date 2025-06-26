78acec4b32340c7cf2dfa92415c0e3b4e3b736fa
Aayush Chalise <chaliseaayush200@gmail.com>
2025-06-26 23:13:20 +0545

    add ping route

--- src/server.js
+++ src/server.js
@@ -28,13 +28,13 @@ app.use(express.json());
 // http://localhost:5003/fanta.css garera browser ma acess garna milxa.
 app.use(express.static(path.join(__dirname, "../public")));
 
+app.get("/ping", (req, res) => {
+  res.send("OK");
+});
+
 // Serving up the HTML file from the public folder
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "index.html"));
-});
-
-app.get("/ping", (req, res) => {
-  res.send("OK");
 });
 
 // ROUTES
