c88e2c20ee0e0ddec4f3a665203b89f3a4a11f4d
Aayush Chalise <chaliseaayush200@gmail.com>
2025-06-26 13:55:09 +0545

    Initial commit for chapter_3 Dockerized app

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 import bcrypt from "bcryptjs"; // used for hashing passwords securely
 import jwt from "jsonwebtoken"; // used to create and verify JWTs for user authentication. n
-import db from "../db.js";
+import prisma from "../prismaClient.js";
 
 const router = express.Router(); //
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const saltRounds = 8; // saltRounds control how many times the hashing algorithm works  . The more round makes more security but decreases speed of the hashing algorithm / Slower performance.
+  const saltRounds = 8; // saltRounds control how many times the hashing algorithm works  . The more round makes more security but decreases speed of the hashing  algorithm / Slower performance.
   // encrypt the password
   const hashedPassword = bcrypt.hashSync(password, saltRounds);
 
@@ -15,24 +15,27 @@ router.post("/register", (req, res) => {
 
   // Error Handling(try and catch)
   try {
-    const insertUser = db.prepare(
-      "INSERT INTO users  (username, password) VALUES (? , ?)"
-    ); // INSERT, UPDATE, DELETE these kind of query returns a object with meta data(data about data). Username and password are the name of the columns  // .prepare() is SQL statement before execution. similar to .exec() method but it is ideal for multiple use and it can have a parameter which .exec() method didnot have. returns a obj which has methods like .get(), .run() etc.
-    const result = insertUser.run(username, hashedPassword); // pass the value we want to store in db.
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
 
     // now that we have a user, I want to add their first todo for them
     const defaultTodo = `Hello :) Add your first todo!`;
-    const insertTodo = db.prepare(
-      `INSERT INTO todos (user_id, task) VALUES(?, ?)`
-    );
-    insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+    const todo = await prisma.todo.create({
+      data: {
+        task: defaultTodo,
+        userId: user.id,
+      },
+    });
 
     //create a token
-    const token = jwt.sign(
-      { id: result.lastInsertRowid },
-      process.env.JWT_SECRET,
-      { expiresIn: "24h" }
-    );
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
 
     res.json({ token });
   } catch (err) {
@@ -42,14 +45,17 @@ router.post("/register", (req, res) => {
   // res.send("Registering user");
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   // We get their email and we look up the password associated with that email in the database . but we get it back and see its encrypted, which means that we cannot compare it to the one the user just used trying to login so what we can to do is again one way encrypt the password the user just entered
 
   const { username, password } = req.body;
 
   try {
-    const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
-    const user = getUser.get(username); // user is a object where it holds username, password and id of a single user not all users, which we get from database
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
 
     if (!user) {
       return res.status(404).send({ message: "User not found" });
